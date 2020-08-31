11e1aa39a501d80d0df9e7b75c2058c4bde67715
WieElang <elangsurya66@gmail.com>
2020-08-31 20:24:56 +0700

    adding post tagging, search and comment form

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
+class Type(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+        
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    post_type = models.ManyToManyField(Type, blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
